b06ff37cc3a9e3559c902b345e4a2dce4c556c63
Nicolas Gonzalez <nico-fgs@hotmail.com>
2024-02-28 23:58:58 -0300

    changed back views.py

--- api/views.py
+++ api/views.py
@@ -3,30 +3,17 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
 
-from django.http import HttpResponse
-import socket
-
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
     def list(self, request):
-        pod_hostname = socket.gethostname()
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        response_data = {
-            'pod_hostname': pod_hostname,
-            'users': serializer.data
-        }
-        return Response(response_data)
+        serializer = self.get_serializer(self.get_queryset(), many = True)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk):
-        pod_hostname = socket.gethostname()
         serializer = self.get_serializer(self.get_object())
-        response_data = {
-            'pod_hostname': pod_hostname,
-            'data': serializer.data
-        }
-        return Response(response_data)
+        return Response(serializer.data)
 
     def create(self, request):
         data = request.data
@@ -38,9 +25,4 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
-        pod_hostname = socket.gethostname()
-        response_data = {
-            'pod_hostname': pod_hostname,
-            'data': serializer.data
-        }
-        return Response(response_data, status=201)
+        return Response(serializer.data, status=201)
